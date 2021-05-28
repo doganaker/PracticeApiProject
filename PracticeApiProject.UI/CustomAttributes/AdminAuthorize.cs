9c1079db1f7b67f47d03327af1213e1cf434abfc
Doğan Aker <dogan234@gmail.com>
2021-05-28 13:10:22 +0300

    Custom Attribute

---
+++ PracticeApiProject.UI/CustomAttributes/AdminAuthorize.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PracticeApiProject.UI.CustomAttributes
+{
+    public class AdminAuthorize : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            if(user == null || !user.Identity.IsAuthenticated || user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role).Value != "Admin")
+            {
+                context.Result = new ChallengeResult(JwtBearerDefaults.AuthenticationScheme);
+            }
+        }
+    }
+}
